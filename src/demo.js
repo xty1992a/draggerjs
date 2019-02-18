e352b27fc3189f2a0495e9eba1ca2a1e7350df3c
谢腾远 <redbuck@126.com>
2019-02-18 15:11:23 +0800

    add: first commit

---
+++ src/demo.js
@@ -0,0 +1,46 @@
+// 未编译API,需要引入polyfill
+import 'core-js'
+import Dragger from './package/dragger'
+
+// 一般用法
+const dragger = new window.Dragger(document.getElementById('drag-wrap'));
+
+// 结合MVVM框架使用
+
+new window.Vue({
+  el: '#app',
+  data() {
+	return {
+	  list: [1, 2, 3, 4],
+	}
+  },
+  mounted() {
+	this.dragger = new Dragger(this.$refs.list, {
+	  change: false,
+	})
+	this.dragger.on('drag-over', pos => {
+	  this.changeItem(pos);
+	  setTimeout(() => {
+		this.dragger.freshThreshold();
+	  }, 20)
+	})
+  },
+  methods: {
+	changeItem({source, target}) {
+	  let list = this.list;
+	  // 取出被拖拽元素
+	  let temp = list.splice(source, 1);
+	  // 截取开头到被交换位置的元素
+	  let start = list.splice(0, target);
+	  // 组装成结果数组
+	  this.list = [...start, ...temp, ...list];
+	},
+  },
+  template: `
+  <div class="app">
+  <ul class="list" ref="list">
+  <li class="item" v-for="it in list" :key="it">item {{it}}</li>
+</ul>
+</div>
+  `,
+});
